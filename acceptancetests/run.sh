aa90e54f942fa34f1f30ee808777afdddfc11b7d
Dan Speed <dan@the-nexus.co.uk>
2015-04-10 23:01:16 +0000

    getting closer

--- acceptancetests/run.sh
+++ acceptancetests/run.sh
@@ -1,8 +1,14 @@
-for f in $TRAVIS_BUILD_DIR/*test.sh
+for f in $TRAVIS_BUILD_DIR/acceptancetests/*test.sh
 do
 	[ ! -f "$f" ] && continue
 
 	echo "Running acceptance test $f"
+	sh $f
+
+	rc=$?
+	if [ $rc != 0 ] then
+		exit $rc
+	fi
 done
 
 
