da2327b93e95bcc88b5cd0f6bd55d5cc12d6e12c
Dan Speed <dan@the-nexus.co.uk>
2015-05-31 23:39:37 +0000

    Quiet Wget in acceptance test

--- acceptancetests/truncated_test.sh
+++ acceptancetests/truncated_test.sh
@@ -1,8 +1,8 @@
 echo "Testing filling in a truncated file"
-wget https://s3-eu-west-1.amazonaws.com/gosync-test/0.1.2/gosync.exe -O test.exe
+wget -q https://s3-eu-west-1.amazonaws.com/gosync-test/0.1.2/gosync.exe -O test.exe
 cp test.exe compare.exe
 gosync b test.exe
-truncate -s 5000000 test.exe  
+truncate -s 5000000 test.exe
 gosync p test.exe test.gosync https://s3-eu-west-1.amazonaws.com/gosync-test/0.1.2/gosync.exe
 diff -q test.exe compare.exe
 rc=$?
